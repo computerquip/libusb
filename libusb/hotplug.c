2515d8ef4b35482bf9d28848ea93c64a445c4f42
Zachary Lund <admin@computerquip.com>
2014-08-29 01:34:50 -0500

    Last commit before bedtime...
    Fixed some segmentation faults in common cases with last commit.
    Move away from driver_match'ing for everything... which especially doesn't make sense
    now that we have relational data between connect and disconnect callback.

--- libusb/hotplug.c
+++ libusb/hotplug.c
@@ -48,7 +48,7 @@ struct hotplug_list {
  * If it matches, we fire off the callback corresponding to the event. 
  */
 static int usbi_hotplug_match_driver(struct libusb_context *ctx,
-	struct libusb_device *dev, libusb_hotplug_event event,
+	struct libusb_device* dev,
 	struct hotplug_list *driver_it)
 {
 	const struct libusb_hotplug *driver = driver_it->driver;
@@ -68,14 +68,21 @@ static int usbi_hotplug_match_driver(struct libusb_context *ctx,
 		return 1;
 	}
 
-	if (event == LIBUSB_HOTPLUG_EVENT_DEVICE_ARRIVED)
-		return driver->connect(ctx, dev);
-	else if (event == LIBUSB_HOTPLUG_EVENT_DEVICE_LEFT) {
-		driver->disconnect(ctx, dev);
-		return 0;
-	}
+
+	return driver->connect(ctx, dev);
+}
+
+static void usbi_hotplug_disconnect_device(struct libusb_context *ctx,
+	struct libusb_device *device,
+	struct hotplug_list *it)
+{
+	struct device_list *device_node;
 	
-	return 2; /* This should never happen.... */
+	list_for_each_entry(device_node, (struct list_head*)&it->device_list, list, struct device_list) {
+		if (device_node->device != device) continue;
+				
+		it->driver->disconnect(ctx, device);
+	}
 }
 
 void usbi_hotplug_match(struct libusb_context *ctx, struct libusb_device *dev,
@@ -86,10 +93,15 @@ void usbi_hotplug_match(struct libusb_context *ctx, struct libusb_device *dev,
 	usbi_mutex_lock(&ctx->hotplug_drivers_lock);
 
 	list_for_each_entry_safe(it, next, &ctx->hotplug_drivers, list, struct hotplug_list) {
-		int error = usbi_hotplug_match_driver(ctx, dev, event, it);
-		
-		if (!error) {
-			list_add(&dev->list, (struct list_head*)&it->device_list);
+		if (event == LIBUSB_HOTPLUG_EVENT_DEVICE_ARRIVED) {
+			int error = usbi_hotplug_match_driver(ctx, dev, it);
+			
+			if (!error) {
+				list_add(&dev->list, (struct list_head*)&it->device_list);
+			}
+		}
+		else if(event == LIBUSB_HOTPLUG_EVENT_DEVICE_LEFT) {
+			usbi_hotplug_disconnect_device(ctx, dev, it);
 		}
 		
 	}
@@ -146,8 +158,11 @@ int API_EXPORTED libusb_hotplug_register(
 		}
 
 		for (i = 0; i < len; i++) {
-			usbi_hotplug_match_driver(ctx, devs[i],
-					LIBUSB_HOTPLUG_EVENT_DEVICE_ARRIVED, node);
+			int error = usbi_hotplug_match_driver(ctx, devs[i], node);
+			
+			if (!error) {
+				list_add(&devs[i]->list, (struct list_head*)&node->device_list);
+			}
 		}
 
 		libusb_free_device_list(devs, 1);
@@ -193,7 +208,7 @@ void usbi_hotplug_deregister_all(struct libusb_context *ctx) {
 	
 	list_for_each_entry_safe(it, next, &ctx->hotplug_drivers, list, struct hotplug_list) {
 		list_for_each_entry(device_node, (struct list_head*)&it->device_list, list, struct device_list) {
-			usbi_hotplug_match_driver(ctx, device_node->device, LIBUSB_HOTPLUG_EVENT_DEVICE_LEFT, it);
+			it->driver->disconnect(ctx, device_node->device);
 		}
 		
 		list_del((struct list_head*)it);
